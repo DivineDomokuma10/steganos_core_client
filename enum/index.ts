ac38defeb063c80fef90e2713fc0930e561278c7
DivineDomokuma10 <divinedomokuma@gmail.com>
2026-05-25 13:48:30 +0100

    fix: api route enum values

--- enum/index.ts
+++ enum/index.ts
@@ -1,14 +1,14 @@
 export enum AUTH_ENDPOINTS {
-  LOGIN = "/auth/login",
-  REFRESH = "/auth/refresh",
-  REGISTER = "/auth/register",
+  LOGIN = "/api/auth/login",
+  REFRESH = "/api/auth/refresh",
+  REGISTER = "/api/auth/register",
 }
 
 export enum USER_ENDPOINTS {
-  PROFILE = "/users/me/",
+  PROFILE = "/api/users/me/",
 }
 
 export enum STEG_ENDPOINTS {
-  ENCODE = "/steg/encode/",
-  DECODE = "/steg/decode/",
+  ENCODE = "/api/steg/encode/",
+  DECODE = "/api/steg/decode/",
 }
